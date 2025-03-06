94ba972d782ecf34c76b6357bb9be2131cb6e6e8
suhail khan <khansohail015@gmail.com>
2025-03-06 19:32:54 +0530

    done

--- src/pages/Homepage/Carousel.jsx
+++ src/pages/Homepage/Carousel.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./Carousel.css";
-import image1 from "../../assets/carusel.png";
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Carousel.css';
+import image1 from '../../assets/carusel.png';
+import image2 from '../../assets/CIM-Setup-Lucknow.webp';
 const Carousel = () => {
   const settings = {
     dots: true,
@@ -19,28 +20,25 @@ const Carousel = () => {
   };
 
   return (
-    <div className="carousel-container">
+    <div className='carousel-container'>
       <Slider {...settings}>
-        <div className="carousel-slide">
-          <img src={image1} alt="image-1" />
-          <div className="carousel-text">
+        <div className='carousel-slide'>
+          <img src={image1} alt='image-1' />
+          <div className='carousel-text'>
             CUTTING-EDGE, USER-FRIENDLY <br />
             TECHNOLOGY INTEGRATED WITH IOT, <br /> AR/VR, AND SIMULATION TOOLS
           </div>
         </div>
-        <div className="carousel-slide">
-          <img
-            src="https://www.cadmech.co.in/images/img/about_us/CIM-Setup-Pune.webp"
-            alt="Slide 2"
-          />
-          <div className="carousel-text">Innovative Solutions</div>
+        <div className='carousel-slide'>
+          <img src={image2} alt='Slide 2' />
+          <div className='carousel-text'>Innovative Solutions</div>
         </div>
-        <div className="carousel-slide">
+        <div className='carousel-slide'>
           <img
-            src="https://www.cadmech.co.in/images/img/about_us/CIM-Setup-Pune.webp"
-            alt="Slide 3"
+            src='https://www.cadmech.co.in/images/img/about_us/CIM-Setup-Pune.webp'
+            alt='Slide 3'
           />
-          <div className="carousel-text">Transforming Ideas into Reality</div>
+          <div className='carousel-text'>Transforming Ideas into Reality</div>
         </div>
       </Slider>
     </div>
